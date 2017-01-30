da625482900bc61a0fda23f3ebe464f99f457a15
Christopher Kim <cj3kim@gmail.com>
2017-01-30 13:31:58 -0800

    Created some initial tests.

---
+++ blackjack_test.js
@@ -0,0 +1,29 @@
+var chai = require("chai")
+var chaiImmutable = require("chai-immutable");
+
+describe("#gen_deck ", function() {
+  it("should create an initial deck of playing cards", function () {
+    var result;
+    expectedResult;
+    assert.deepEqual(result, expectedResult)
+  });
+})
+
+describe("#score_cards_in_hand ", function() {
+  describe("when cards in hand are over 21", ,function () {
+    it("should return BUST state", function () {
+      var result;
+      expectedResult;
+
+      assert.deepEqual(result, expectedResult)
+    });
+  })
+  describe("when cards in hand sum to 21 and under", ,function () {
+    it("should return the cards and score", function () {
+      var result;
+      expectedResult;
+      assert.deepEqual(result, expectedResult)
+    });
+  })
+
+})
