09aa5652833c6bd51a30a085b17315359117260b
Christopher Kim <cj3kim@gmail.com>
2017-01-30 14:59:49 -0800

    Finished testing #score_cards_in_hand function

--- blackjack.js
+++ blackjack.js
@@ -22,7 +22,42 @@ function generate_deck (suits) {
   }).flatten(true);
 
 }
+function isScoreValid (score) {
+  return score > 21 ? false : true;
+}
+
+function score_cards_in_hand (_hand) {
+  var hand = List(_hand);
+
+  var aceKey = hand.findKey(function (e) { return e.cardName === "Ace" });
+  var hasAce = _.isNumber(aceKey);
+
+  if (hasAce) {
+    var aceCard = hand.get(aceKey);
+    var acelessHand = hand.delete(aceKey);
+
+    var computedScores = aceCard.value.map(function (k) {
+      var sumOfOtherCards = acelessHand.reduce(function (accum, e) { return accum += e.value; }, 0)
+      var _score =  k + sumOfOtherCards;
+      return _score;
+    });
+
+    var validScores = List(computedScores.filter(isScoreValid));
+
+    if (validScores.size === 0) {
+      return {score: computedScores.min(), valid: false }
+    } else {
+      return {score: validScores.max(), valid: true}
+    }
+  } else {
+    var score = hand.reduce(function(accum, card) { return accum += card.value }, 0);
+    var valid = isScoreValid(score);
+    return {score: score, valid: valid};
+  }
+}
 
 module.exports = {
-  generate_deck: generate_deck
+  generate_deck: generate_deck,
+  score_cards_in_hand: score_cards_in_hand
 }
+
